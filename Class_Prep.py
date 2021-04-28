e9f5caac85898331a364d0f29ae84c628ce40d1a
editingbay <desmondafoster@gmail.com>
2021-04-27 20:14:54 -0400

    Pi Tennis Ver 0.8.2 with introduction of classes? Not sure how they work yet.

--- Class_Prep.py
+++ Class_Prep.py
@@ -10,39 +10,24 @@ GPIO.setup(29, GPIO.OUT)    # direction control b.1
 GPIO.setup(31, GPIO.OUT)    # direction control b.2
 
 
-
-
 class ShotType:
-    def __init__(self, motor1, motor2, servo1, servo2, direction, description):
-        self.motor1 = motor1
-        self.motor2 = motor2
-        self.servo1 = servo1
-        self.servo2 = servo2
+    def __init__(self, m1, m2, s1, s2, direction, description):
+        self.m1 = m1
+        self.m2 = m2
+        self.s1 = s1
+        self.s2 = s2
         self.direction = direction
         self.description = description
 
-class GpioPwm:
-    var1 = GPIO.PWM(11, ShotType.motor1)
-    var2 = GPIO.PWM(13, ShotType.motor2)
-    var3 = GPIO.PWM(16, ShotType.direction)
-    var4 = GPIO.PWM(18, ShotType.direction)
-    var5 = GPIO.PWM(29, ShotType.direction)
-    var6 = GPIO.PWM(31, ShotType.direction)
+
+def pwm(pin, freq):
+    GPIO.PWM(pin, freq)
 
 
-
-class PwmMotor:
-    def __init__(self, ):
-
-
-
-
+pwm(11, 30)
+pwm(13, 100)
 
 
 s1 = ShotType(50, 50, 0, 0, "Forward", "Center, Center")
 s2 = ShotType(50, 0, 0, 0, "Forward", "Left, Center")
 s3 = ShotType(50, 100, 0, 0, "Forward", "Right, Center")
-
-s1.motor1
-s1.name
-
